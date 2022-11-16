a7e42fe64b33850af7993dd954537a5fa6896624
hamzah andy <hamzahhamah@gmail.com>
2022-11-16 22:00:00 +0700

    first commit

---
+++ application/views/admin/index.php
@@ -0,0 +1,13 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <h4>selamat datang:</h4>
+    <h3><?= $user['name']; ?></h3>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
